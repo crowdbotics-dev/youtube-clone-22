f870587227d206633217690747196d0de5b13f12
Crowdbotics <engineering@crowdbotics.com>
2023-07-07 15:57:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "youtube-clone-22",
+  url: "https://youtube-clone-22.botics.co",
+  api: "https://youtube-clone-22.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
